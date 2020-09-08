96d3ae020926cb6e5423e1397eebbb82d998e51e
Chad Harris <chadwickhar08@gmail.com>
2020-09-08 17:46:35 -0400

    Adding the play2.js changes.

--- play2.js
+++ play2.js
@@ -9,3 +9,45 @@ const person = {
 };
 
 person.greet();
+
+const hobbies = ['Sports', 'Cooking'];
+
+for(let hobby of hobbies){
+
+    console.log(hobby);
+
+}
+
+const printName = ({name}) => {
+
+    console.log(name);
+
+};
+
+printName(person);
+
+const {name, age} = person;
+console.log(name, age);
+
+// const copiedPerson = {...person};
+
+// console.log(copiedPerson);
+
+// const toArray = (...args) => {
+
+//     return args;
+
+// };
+
+// console.log(toArray(1,2,3,4));
+
+// // console.log(hobbies.map(hobby=>{
+// //     return 'Hobby: ' + hobby;
+// // }));
+
+// // console.log(hobbies);
+
+// // hobbies.push("Programming");
+// // console.log(hobbies);
+// const copiedArray = [...hobbies]
+// console.log(copiedArray);
