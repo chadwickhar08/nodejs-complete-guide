0ba515ea0e1f5de079e2c94c6e85e5b251b3d9fe
Chad Harris <chadwickhar08@gmail.com>
2020-10-04 10:02:42 -0400

    Adding express and express code.

--- App/app.js
+++ App/app.js
@@ -1,11 +1,26 @@
-const http = require('http');
 
-const routes = require('./routes');
-
-console.log(routes.someText);
+const express = require('express');
 
 
-const server = http.createServer(routes.handler);
+const app = express();
 
+app.use('/', (req, res, next) => {
 
-server.listen(3000);
+    console.log("This always runs!");
+    next();
+
+});
+
+app.use('/add-product',(req, res, next) => {
+
+    //console.log("In another middleware");
+    res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/',(req, res, next) => {
+
+    //console.log("In another middleware");
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
