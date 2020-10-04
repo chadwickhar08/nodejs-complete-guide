d5aa2669d850348f63875ccbf18479e2e307fa38
Chad Harris <chadwickhar08@gmail.com>
2020-10-04 11:38:24 -0400

    Adding new files with routes for correct navigation.

--- App/app.js
+++ App/app.js
@@ -1,26 +1,24 @@
 
 const express = require('express');
+const path = require('path');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
-app.use('/', (req, res, next) => {
+app.use(bodyParser.urlencoded({extended:false}));
 
-    console.log("This always runs!");
-    next();
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-});
+app.use((req, res, next) => {
 
-app.use('/add-product',(req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 
-    //console.log("In another middleware");
-    res.send('<h1>The "Add Product" Page</h1>');
-});
-
-app.use('/',(req, res, next) => {
-
-    //console.log("In another middleware");
-    res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
