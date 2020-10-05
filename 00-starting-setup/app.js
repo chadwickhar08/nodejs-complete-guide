ea3bc771e45c45fadf85d0eaa5b54c6344ad9b4a
Chad Harris <chadwickhar08@gmail.com>
2020-10-05 04:56:45 -0400

    Adding the hbs main layout and templates.

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -6,7 +6,7 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'}));
+app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
 app.set('view engine', 'hbs');
 //app.set('view engine', 'pug');
 app.set('views', 'views');
@@ -21,7 +21,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {docTitle: "Page Not Found", layout: false});
+    res.status(404).render('404', {docTitle: "Page Not Found"});
 });
 
 app.listen(3000);
--- 00-starting-setup/routes/shop.js
+++ 00-starting-setup/routes/shop.js
@@ -10,7 +10,11 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   //console.log('shop.js', adminData.products);
   const products = adminData.products;
-  res.render('shop', {prods: products, docTitle: "Shop", path: '/', hasProducts: products.length > 0, layout: false});
+  res.render('shop', {prods: products, docTitle: "Shop", 
+  path: '/', 
+  hasProducts: products.length > 0, 
+  activeShop: true,
+  productCSS: true});
   //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
