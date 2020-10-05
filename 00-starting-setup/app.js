e84a86b5f103e9dbbaa0bccf5ccf67e2ffbd595d
Chad Harris <chadwickhar08@gmail.com>
2020-10-05 08:31:56 -0400

    Changed the path for 404 error.

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -22,7 +22,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {docTitle: "Page Not Found"});
+    res.status(404).render('404', {docTitle: "Page Not Found", path: "/not-found"});
 });
 
 app.listen(3000);
