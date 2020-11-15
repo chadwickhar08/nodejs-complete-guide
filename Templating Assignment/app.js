d03ffb340bb5d4844ce317afd777ecc7b4268046
Chad Harris <chadwickhar08@gmail.com>
2020-11-15 18:08:34 -0500

    Adding the assignment for templating.

---
+++ Templating Assignment/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.get('/', (req, res, next) => {
+    res.render();
+});
+
+app.get('/users', (req, res, next) => {
+    res.render();
+});
+
+app.get('/users', (req, res, next) => {
+    res.render();
+});
+
+app.post('/add-user', (req, res, next) => {
+    res.redirect('/users');
+});
+
+
+app.listen(3000);
