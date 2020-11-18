def3f92e419bd30da6950800cffcac8383a08bc9
Chad Harris <chadwickhar08@gmail.com>
2020-11-17 20:31:47 -0500

    Adding the written project files completed for the first assignment.

--- Templating Assignment/app.js
+++ Templating Assignment/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
 
 const app = express();
 
 const users = [];
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs({defaultLayout: 'main-layout', extname: 'hbs'}));
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/users', (req, res, next) => {
-    res.render('users', { pageTitle: 'User', users: users });
+    res.render('users', { pageTitle: 'User', users: users, hasUsers: users.length > 0 });
   });
 
 app.get('/', (req, res, next) => {
