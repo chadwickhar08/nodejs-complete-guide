3deb8c1628c75517916b8026872f0d5a9e000ad5
Chad Harris <chadwickhar08@gmail.com>
2020-11-18 09:29:03 -0500

    Adding the 404 controller.

--- 00-starting-setup/app.js
+++ 00-starting-setup/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 //const expressHbs = require('express-handlebars');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 //app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}));
@@ -21,8 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {docTitle: "Page Not Found", path: "/not-found"});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
