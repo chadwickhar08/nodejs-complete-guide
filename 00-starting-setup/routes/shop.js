0836d910e8257094825fd96a3fc4915a5388ee92
Chad Harris <chadwickhar08@gmail.com>
2020-11-22 09:59:20 -0500

    Changed the order of the route in shop.js for the root dir so that it is first.

--- 00-starting-setup/routes/shop.js
+++ 00-starting-setup/routes/shop.js
@@ -11,12 +11,13 @@ const router = express.Router();
 
 
 
+router.get('/', shopController.getIndex);
+
 router.get('/products', shopController.getProducts);
 
 router.get('/cart', shopController.getCart);
 
 router.get('/checkout', shopController.getCheckout);
 
-router.get('/', shopController.getIndex);
 
 module.exports = router;
