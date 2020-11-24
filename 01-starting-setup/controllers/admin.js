58bc7f03f9839145d3c3fc7fcee9899c0855fc0a
Chad Harris <chadwickhar08@gmail.com>
2020-11-24 08:30:23 -0500

    Making some changes to the path for adding/editing product.

--- 01-starting-setup/controllers/admin.js
+++ 01-starting-setup/controllers/admin.js
@@ -3,7 +3,7 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
-    path: '/admin/add-product',
+    path: '/admin/edit-product',
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true
@@ -27,8 +27,8 @@ exports.getEditProduct = (req, res, next) => {
     res.redirect('/');
   }
   res.render('admin/edit-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
+    pageTitle: 'Edit Product',
+    path: '/admin/edit-product',
     editing: editMode
   });
 };
