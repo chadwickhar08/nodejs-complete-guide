fe2c629a48178f895dc02f8a5b9ea3562a346be7
Chad Harris <chadwickhar08@gmail.com>
2020-11-24 08:18:01 -0500

    Adding the editMode boolean to check query string in url within the controller.

--- 01-starting-setup/controllers/admin.js
+++ 01-starting-setup/controllers/admin.js
@@ -23,10 +23,13 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
+  if(!editMode){
+    res.redirect('/');
+  }
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    editing: true
+    editing: editMode
   });
 };
 
