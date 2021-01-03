69ac184283f06f76fbf4710eafba5cccc48d7675
Chad Harris <chadwickhar08@gmail.com>
2021-01-03 17:20:29 -0500

    Adding the delete functionality to cart.

--- 01-starting-setup/controllers/shop.js
+++ 01-starting-setup/controllers/shop.js
@@ -69,6 +69,11 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
 
   const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
+  
 
 }
 
