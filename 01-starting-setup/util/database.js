9d836ddfcffd847bf75ce1b0acd654cc812f6c65
Chad Harris <chadwickhar08@gmail.com>
2021-03-05 08:18:03 -0500

    Adding updated files for web app.

--- 01-starting-setup/util/database.js
+++ 01-starting-setup/util/database.js
@@ -1,12 +1,19 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
 
-    host:'localhost',
-    user: 'user1',
-    database: 'node-complete',
-    password: 'greenR08!',
-    port: 3306
-});
+//     host:'localhost',
+//     user: 'user1',
+//     database: 'node-complete',
+//     password: 'greenR08!',
+//     port: 3306
+// });
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-complete', 'user1', 'greenR08!', {dialect: 'mysql', host: 'localhost'});
+
+module.exports = sequelize;
+
