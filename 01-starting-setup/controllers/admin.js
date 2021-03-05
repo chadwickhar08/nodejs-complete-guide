dbccf6f01dc082b73b8e3f00af938a3d39eb694a
Chad Harris <chadwickhar08@gmail.com>
2021-03-05 08:37:21 -0500

    Adding update dev code to work with sequelize

--- 01-starting-setup/controllers/admin.js
+++ 01-starting-setup/controllers/admin.js
@@ -13,10 +13,18 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save().then(() => {
-    res.redirect('/');
-  }).catch(err => {console.log(err);});
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  }).then(result => {
+
+    console.log('Created product');
+
+  }).catch(err => {
+    console.log(err);
+  });
   
 };
 
@@ -52,13 +60,17 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.findAll().then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+    }).catch(err => {
+
+      console.log(err);
+
+    });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
--- 01-starting-setup/models/product.js
+++ 01-starting-setup/models/product.js
@@ -1,44 +1,77 @@
-const cart = require('./cart');
-const db = require('../util/database');
+// const cart = require('./cart');
+// const db = require('../util/database');
 
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
-};
+// const getProductsFromFile = cb => {
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     } else {
+//       cb(JSON.parse(fileContent));
+//     }
+//   });
+// };
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-    //adding a comment to update git repo
-  }
+// module.exports = class Product {
+//   constructor(id, title, imageUrl, description, price) {
+//     this.id = id;
+//     this.title = title;
+//     this.imageUrl = imageUrl;
+//     this.description = description;
+//     this.price = price;
+//     //adding a comment to update git repo
+//   }
 
-  save() {
-    return db.execute('INSERT INTO products2 (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
-    [this.title, this.price, this.description, this.imageUrl]
+//   save() {
+//     return db.execute('INSERT INTO products2 (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
+//     [this.title, this.price, this.description, this.imageUrl]
     
-    );
-  }
+//     );
+//   }
 
-  static deleteById(id) {
+//   static deleteById(id) {
 
-  }
+//   }
 
-  static fetchAll() {
-    return db.execute('SELECT * FROM products2');
+//   static fetchAll() {
+//     return db.execute('SELECT * FROM products2');
 
-  }
+//   }
 
-  static findById(id, cb) {
-    return db.execute('SELECT * FROM products2 WHERE products2.id = ?', [id]);
+//   static findById(id, cb) {
+//     return db.execute('SELECT * FROM products2 WHERE products2.id = ?', [id]);
    
+//   }
+// };
+
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product', {
+
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type:Sequelize.STRING,
+    allowNull: false
   }
-};
+
+});
+
+module.exports = Product;
+
+
+
