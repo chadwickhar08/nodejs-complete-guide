cec214e7d76afc95a06d16e03d968b80cc7516e2
Chad Harris <chadwickhar08@gmail.com>
2021-03-07 20:05:01 -0500

    Adding updated product files.

--- 01-starting-setup/app.js
+++ 01-starting-setup/app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item')
 
 const app = express();
 
@@ -15,6 +17,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { postAddProduct } = require('./controllers/admin');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -38,6 +41,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 
 sequelize.sync().then(result => {
@@ -54,6 +61,9 @@ sequelize.sync().then(result => {
 
 }).then(user => {
     //console.log(user);
+    return user.createCart();
+    
+}).then(cart => {
     app.listen(3000);
 })
 .catch(err => {
