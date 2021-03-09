16753e28ced19bf87531ff8a083fd7c28f20730f
Chad Harris <chadwickhar08@gmail.com>
2021-03-09 17:25:09 -0500

    Adding mongodb changes.

--- 01-starting-setup/app.js
+++ 01-starting-setup/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 // const shopRoutes = require('./routes/shop');
 const { postAddProduct } = require('./controllers/admin');
 
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 
 });
 
-// app.use('/admin', adminRoutes);
+app.use('/admin', adminRoutes);
 // app.use(shopRoutes);
 
 app.use(errorController.get404);
