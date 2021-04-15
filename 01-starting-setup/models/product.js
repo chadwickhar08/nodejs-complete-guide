5cd3f664b334df4a4e144cecf9f860b1b62e0c42
Chad Harris <chadwickhar08@gmail.com>
2021-04-15 10:12:14 -0400

    Adding the updated files.

--- 01-starting-setup/models/product.js
+++ 01-starting-setup/models/product.js
@@ -1,3 +1,28 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    require: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    require: true
+  }
+});
+
+module.exports = mongoose.model()
+
 // const cart = require('./cart');
 // const db = require('../util/database');
 
